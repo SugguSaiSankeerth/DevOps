b034cb9e5a02bf1cfe47f162a1ab6c42a6d3d481
PVK16 <Vamshi.Krishna@iiitb.org>
2019-04-24 22:51:06 +0530

    Removed Comments

--- src/main/java/org/iiitb/DevOps/dbcon/DatabaseConnection.java
+++ src/main/java/org/iiitb/DevOps/dbcon/DatabaseConnection.java
@@ -13,7 +13,6 @@ public class DatabaseConnection {
 
 	public static Connection getConnection() {
 		try {
-//			Statement stmt;
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");			
 			String user = "root";
@@ -22,28 +21,10 @@ public class DatabaseConnection {
 			conn = DriverManager.getConnection("jdbc:mysql://db:3306/spectrumdata?autoReconnect=true&useSSL=false",
 					user, pass);
 			System.out.println("Connections Success");
-//			stmt = conn.createStatement();
-//			String sql;
-//			sql = "SELECT * from seller";
-//			ResultSet rs = stmt.executeQuery(sql);
-//			while (rs.next()) {
-//				String fname = rs.getString("fname");
-//				System.out.print("fname: " + fname);
-//			}
-//			
-//	        String query = "select * from sellerLogin where loginId = '"+"asd"+"'and password = '"+"asd"+"'";
-//	        stmt = conn.createStatement();
-//	        rs = stmt.executeQuery(query);
-//	        //return true;
-//	        int count=0;
-//	        while(rs.next())
-//	        {
-//	            count=count+1;
-//	        }
 	    }
 		catch (Exception e) {
 			System.out.println("in catch");
-		//e.printStackTrace();
+		
 	}
 		return conn;
 	}

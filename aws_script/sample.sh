73ec424d40be9f70411f79a4d0f6af50e6cca470
Rakshith Potluri <raks_097@hotmail.com>
2019-04-25 13:10:36 +0530

    Added Shell Script to generate AWS Resource.XML

---
+++ aws_script/sample.sh
@@ -0,0 +1,18 @@
+
+echo -e "\nConfiguring aws tool\n"  
+aws configure < aws_script/configFile.txt
+
+echo -e "\n\nGenerating Resource XML File\n"
+publicIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=docker_elk" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<project>
+<node name=\"spectrum_ec2_aws\" description=\"Spectrum Website\" tags=\"\"
+  hostname=\"$publicIP\" osArch=\"amd64\" osFamily=\"unix\"
+  osName=\"Linux\" osVersion=\"4.15.0-1032-aws\"
+  username=\"ubuntu\" ssh-authentication=\"privateKey\"
+    ssh-key-storage-path=\"keys/ec2-key\"
+/>
+</project>" > aws_script/docker_elk_resources.xml
+
+echo "Done."
